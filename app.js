5e0f844405eef55b3dd09998fe0f9da2a80f95d4
Harris Joshua <harrisjo1810@gmail.com>
2024-02-08 22:35:06 +0530

    added the signup page

--- app.js
+++ app.js
@@ -20,3 +20,23 @@ function events() {
 }
 
 events();
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the buttons
+  var signUpButton = document.querySelector(".btn");
+  var signInButton = document.querySelector(".signin");
+  var getStartedButton = document.querySelector(".cta .btn");
+
+  // Add event listeners to the buttons
+  signUpButton.addEventListener("click", function () {
+    window.location.href = "signup.html";
+  });
+
+  signInButton.addEventListener("click", function () {
+    window.location.href = "signup.html";
+  });
+
+  getStartedButton.addEventListener("click", function () {
+    window.location.href = "signup.html"; // Assuming get started button leads to sign up
+  });
+});
